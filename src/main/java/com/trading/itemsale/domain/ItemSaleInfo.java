f0f6dc0e6d3a2351cc15161a6c7f9650aa43332d
tlqkrus012345 <tlqkrus012345@naver.com>
2024-09-03 17:06:46 +0900

    판매 등록 아이템 엔티티 컬럼 추가

--- src/main/java/com/trading/itemsale/domain/ItemSaleInfo.java
+++ src/main/java/com/trading/itemsale/domain/ItemSaleInfo.java
@@ -1,6 +1,7 @@
 package com.trading.itemsale.domain;
 
 import com.trading.common.BaseEntity;
+import com.trading.item.domain.ItemType;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -20,6 +21,18 @@ public class ItemSaleInfo extends BaseEntity {
     private Long inventoryId;
 
     private Long itemId;
+
+    private String memberName;
+
+    private String itemName;
+
+    private int itemPrice;
+
+    private ItemType itemType;
+
+    private String description;
+
+    private int quantity;
 
     @Enumerated(value = EnumType.STRING)
     private ItemSaleInfoStatus status;
