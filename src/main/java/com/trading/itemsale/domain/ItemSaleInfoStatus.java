d73e87d16cd499bd2c650d0fa730cb56e6674253
tlqkrus012345 <tlqkrus012345@naver.com>
2024-09-03 18:25:20 +0900

    판매대기 타입 추가

--- src/main/java/com/trading/itemsale/domain/ItemSaleInfoStatus.java
+++ src/main/java/com/trading/itemsale/domain/ItemSaleInfoStatus.java
@@ -3,12 +3,19 @@ package com.trading.itemsale.domain;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @RequiredArgsConstructor
 public enum ItemSaleInfoStatus {
 
     SELLING("판매중"),
-    SOLD_OUT("판매완료");
+    SOLD_OUT("판매완료"),
+    WAIT("판매대기");
 
     private final String explain;
+
+    public static List<ItemSaleInfoStatus> forDisplay() {
+        return List.of(SELLING, SOLD_OUT);
+    }
 }
