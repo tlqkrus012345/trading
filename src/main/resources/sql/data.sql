d5173db60f77315b8bae53073c28b0bf8326dd64
tlqkrus012345 <tlqkrus012345@naver.com>
2024-09-03 18:21:14 +0900

    판매 등록 아이템 데이터

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,4 @@
+insert into item_sale_info(member_id, inventory_id, item_id, member_name, item_name, item_price, item_type, description, status, quantity)
+values (1, 1, 1, '유저1', '강철검', 1000, 'SWORD', '일반 강철검', 'SELLING', 1),
+       (2, 2, 2, '유저2', '목검', 500, 'SWORD', '일반 목검', 'SOLD_OUT', 2),
+       (3, 3, 3, '유저3', '다이아검', 2000, 'SWORD', '일반 다이아검', 'WAIT', 3);
