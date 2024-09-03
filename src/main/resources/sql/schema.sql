f420d23c57139cd1933103fa3924bdda8aa72c49
tlqkrus012345 <tlqkrus012345@naver.com>
2024-09-03 18:20:53 +0900

    schema.sql 생성, 수정 컬럼 수정

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE `member`
     `name`         VARCHAR(20)   NOT NULL COMMENT '회원 닉네임',
     `phone`        VARCHAR(15)   NOT NULL COMMENT '회원 핸드폰 번호',
     `point`        BIGINT        NOT NULL DEFAULT 0 COMMENT '회원 포인트',
-    `created_at`   DATETIME(6)   NOT NULL COMMENT '생성 날짜',
-    `modified_at`  DATETIME(6)   NOT NULL COMMENT '수정 날짜',
+    `created_at`     DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 날짜',
+    `modified_at`    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 날짜',
     PRIMARY KEY (`id`),
     UNIQUE KEY (`phone`, `name`)
 );
@@ -30,8 +30,8 @@ CREATE TABLE `inventory`
     `id`            BIGINT        NOT NULL AUTO_INCREMENT COMMENT '인벤토리 ID',
     `member_id`     BIGINT        NOT NULL COMMENT '회원 ID',
     `item_id`       BIGINT        NOT NULL COMMENT '아이템 ID',
-    `created_at`    DATETIME(6)   NOT NULL COMMENT '생성 날짜',
-    `modified_at`   DATETIME(6)   NOT NULL COMMENT '수정 날짜',
+    `created_at`     DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 날짜',
+    `modified_at`    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 날짜',
     PRIMARY KEY (`id`)
 );
 
@@ -43,8 +43,8 @@ CREATE TABLE `item`
     `type`          VARCHAR(20)    NOT NULL  COMMENT '아이템 타입',
     `price`         INT            NOT NULL COMMENT '아이템 가격',
     `description`   VARCHAR(255)   COMMENT '아이템 설명',
-    `created_at`    DATETIME(6)    NOT NULL COMMENT '생성 날짜',
-    `modified_at`   DATETIME(6)    NOT NULL COMMENT '수정 날짜',
+    `created_at`     DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 날짜',
+    `modified_at`    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 날짜',
     PRIMARY KEY (`id`)
 );
 
@@ -55,8 +55,8 @@ CREATE TABLE `stock`
     `inventory_id`   BIGINT        NOT NULL COMMENT '인벤토리 ID',
     `item_id`        BIGINT        NOT NULL COMMENT '아이템 ID',
     `quantity`       INT           NOT NULL COMMENT '재고 수량',
-    `created_at`     DATETIME(6)   NOT NULL COMMENT '생성 날짜',
-    `modified_at`    DATETIME(6)   NOT NULL COMMENT '수정 날짜',
+    `created_at`     DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 날짜',
+    `modified_at`    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 날짜',
     PRIMARY KEY (`id`)
 );
 
@@ -74,8 +74,8 @@ CREATE TABLE `item_sale_info`
     `description`    VARCHAR(255)  COMMENT '아이템 설명',
     `quantity`       INT           NOT NULL COMMENT '재고 수량',
     `status`         VARCHAR(20)   NOT NULL COMMENT '판매 상태',
-    `created_at`     DATETIME(6)   NOT NULL COMMENT '생성 날짜',
-    `modified_at`    DATETIME(6)   NOT NULL COMMENT '수정 날짜',
+    `created_at`     DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 날짜',
+    `modified_at`    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 날짜',
     PRIMARY KEY (`id`)
 );
 
@@ -90,8 +90,8 @@ CREATE TABLE `item_transaction`
     `total_quantity` INT           NOT NULL COMMENT '총 구매 수량',
     `status`         VARCHAR(20)   NOT NULL COMMENT '거래 상태',
     `ordered_at`     DATETIME(6)   NOT NULL COMMENT '주문 날짜',
-    `created_at`     DATETIME(6)   NOT NULL COMMENT '생성 날짜',
-    `modified_at`    DATETIME(6)   NOT NULL COMMENT '수정 날짜',
+    `created_at`     DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 날짜',
+    `modified_at`    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 날짜',
     PRIMARY KEY (`id`),
     KEY `idx_ordered_at` (`ordered_at`)
 );
