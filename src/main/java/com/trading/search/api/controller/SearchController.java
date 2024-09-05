8ed09146fb19e5092b09c7cd1864c884d1e8ee58
tlqkrus012345 <tlqkrus012345@naver.com>
2024-09-05 15:27:00 +0900

    #2 SearchService -> SearchFacadeService 변경

--- src/main/java/com/trading/search/api/controller/SearchController.java
+++ src/main/java/com/trading/search/api/controller/SearchController.java
@@ -1,9 +1,11 @@
 package com.trading.search.api.controller;
 
 import com.trading.search.api.service.ItemSearchResponse;
-import com.trading.search.api.service.SearchService;
+import com.trading.search.api.service.SearchFacadeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -11,7 +13,7 @@ import java.util.List;
 @RestController
 public class SearchController {
 
-    private final SearchService searchService;
+    private final SearchFacadeService searchService;
 
     @GetMapping("/api/v1/search/{keyword}")
     public List<ItemSearchResponse> searchItemKeyword(@PathVariable("keyword") String keyword) {
