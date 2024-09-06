effa9843deffbbbf69f0187b0699c3f768b0f852
tlqkrus012345 <tlqkrus012345@naver.com>
2024-09-06 14:01:28 +0900

    인기 키워드 리스트 조회 기능

--- src/main/java/com/trading/search/domain/ItemSearchKeywordRepository.java
+++ src/main/java/com/trading/search/domain/ItemSearchKeywordRepository.java
@@ -1,10 +1,15 @@
 package com.trading.search.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ItemSearchKeywordRepository extends JpaRepository<ItemSearchKeyword, Long> {
 
     Optional<ItemSearchKeyword> findByItemKeyword(String keyword);
+
+    @Query(value = "select * from item_search_keyword i order by i.item_search_count desc, i.item_keyword asc limit 3", nativeQuery = true)
+    List<ItemSearchKeyword> findPopularItemKeyword();
 }
--- src/test/java/com/trading/search/domain/ItemSearchKeywordRepositoryTest.java
+++ src/test/java/com/trading/search/domain/ItemSearchKeywordRepositoryTest.java
@@ -1,24 +1,27 @@
 package com.trading.search.domain;
 
+import com.trading.search.api.service.SearchService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.tuple;
 
 @ActiveProfiles("test")
-@DataJpaTest
+@SpringBootTest
 class ItemSearchKeywordRepositoryTest {
 
     @Autowired
     private ItemSearchKeywordRepository itemSearchKeywordRepository;
+
+    @Autowired
+    private SearchService searchService;
 
     @DisplayName("키워드를 통해 itemSearchKeyword 조회한다")
     @Test
@@ -33,5 +36,58 @@ class ItemSearchKeywordRepositoryTest {
 
         assertThat(savedItemSearchKeyword).isPresent();
         assertThat(savedItemSearchKeyword.get().getItemKeyword()).isEqualTo(itemKeyword);
+    }
+
+    @DisplayName("가장 많이 검색된 키워드 3개를 조회한다.")
+    @Test
+    void findPopularItemKeyword() {
+        Long searchCount1 = 4L;
+        Long searchCount2 = 3L;
+        Long searchCount3 = 2L;
+        keywordSearch("레어", searchCount1);
+        keywordSearch("일반", searchCount2);
+        keywordSearch("에픽", searchCount3);
+
+        List<ItemSearchKeyword> popularItemKeywords = itemSearchKeywordRepository.findPopularItemKeyword();
+
+        assertThat(popularItemKeywords).hasSize(3)
+                .extracting("itemKeyword", "itemSearchCount")
+                .containsExactlyInAnyOrder(
+                        tuple("레어", searchCount1),
+                        tuple("일반", searchCount2),
+                        tuple("에픽", searchCount3)
+                );
+        assertThat(popularItemKeywords.get(0).getItemKeyword()).isEqualTo("레어");
+        assertThat(popularItemKeywords.get(1).getItemKeyword()).isEqualTo("일반");
+    }
+
+    @DisplayName("동일한 카운트이면 한글 순으로 정렬된다")
+    @Test
+    void findPopularItemKeyword2() {
+        Long searchCount1 = 2L;
+        Long searchCount2 = 2L;
+        Long searchCount3 = 2L;
+        keywordSearch("나다", searchCount1);
+        keywordSearch("가나", searchCount2);
+        keywordSearch("다가", searchCount3);
+
+        List<ItemSearchKeyword> popularItemKeywords = itemSearchKeywordRepository.findPopularItemKeyword();
+
+        assertThat(popularItemKeywords).hasSize(3)
+                .extracting("itemKeyword", "itemSearchCount")
+                .containsExactlyInAnyOrder(
+                        tuple("가나", searchCount1),
+                        tuple("나다", searchCount2),
+                        tuple("다가", searchCount3)
+                );
+        assertThat(popularItemKeywords.get(0).getItemKeyword()).isEqualTo("가나");
+        assertThat(popularItemKeywords.get(1).getItemKeyword()).isEqualTo("나다");
+        assertThat(popularItemKeywords.get(2).getItemKeyword()).isEqualTo("다가");
+    }
+
+    private void keywordSearch(String keyword, Long count) {
+        for (int i = 0; i < count; i++) {
+            searchService.searchItemKeyword(keyword);
+        }
     }
 }
