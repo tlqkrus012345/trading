d377bd8644aca8906400200f629c18c54fda25d2
tlqkrus012345 <tlqkrus012345@naver.com>
2024-09-06 14:01:51 +0900

    키워드 데이터 추가

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -7,4 +7,13 @@ values (1, 1, 1, '유저1', '강철검', 1000, 'SWORD', '일반 강철검', 'SEL
 insert into item(name, type, price, description)
 values ('강철검', 'SWORD', 7000, '일반 강철검'),
        ('강철도끼', 'AXE', 1500, '일반 강철도끼'),
+       ('great sword', 'SWORD', 15000, '대단한 검'),
        ('다이아창', 'SPEAR', 3000, '레어 다이아창');
+
+insert into item_search_keyword(item_keyword, item_search_count, version)
+values ('가 창', 3, 1),
+       ('나 검', 10, 1),
+       ('라 검', 4, 1),
+       ('다 도끼', 4, 1),
+       ('마 검', 2, 1),
+       ('바 도끼', 1, 1);
