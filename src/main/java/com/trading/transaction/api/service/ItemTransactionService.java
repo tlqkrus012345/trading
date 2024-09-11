1a12df1ce91064e5515eb3c718c358375034929d
tlqkrus012345 <tlqkrus012345@naver.com>
2024-09-11 17:28:35 +0900

    아이템 거래 생성 기능 회원 포인트 사용 기능 추가 및 테스트 수정

--- src/main/java/com/trading/transaction/api/service/ItemTransactionService.java
+++ src/main/java/com/trading/transaction/api/service/ItemTransactionService.java
@@ -1,5 +1,6 @@
 package com.trading.transaction.api.service;
 
+import com.trading.member.api.service.MemberService;
 import com.trading.transaction.domain.ItemTransaction;
 import com.trading.transaction.domain.ItemTransactionRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +14,14 @@ import java.time.LocalDateTime;
 public class ItemTransactionService {
 
     private final ItemTransactionRepository itemTransactionRepository;
+    private final MemberService memberService;
 
     @Transactional
     public ItemTransactionResponse createItemTransaction(ItemTransactionRequest request, LocalDateTime now) {
         ItemTransaction itemTransaction = ItemTransaction.create(request, now);
 
-        return ItemTransactionResponse.from(itemTransactionRepository.save(itemTransaction));
+        Long afterMemberPoint = memberService.payPoint(itemTransaction.getMemberId(), itemTransaction.getTotalPriceWithCharge());
+
+        return ItemTransactionResponse.of(itemTransactionRepository.save(itemTransaction), afterMemberPoint);
     }
 }
--- src/test/java/com/trading/transaction/api/service/ItemTransactionServiceTest.java
+++ src/test/java/com/trading/transaction/api/service/ItemTransactionServiceTest.java
@@ -1,5 +1,9 @@
 package com.trading.transaction.api.service;
 
+import com.trading.member.domain.Member;
+import com.trading.member.domain.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -17,6 +21,18 @@ class ItemTransactionServiceTest {
 
     @Autowired
     private ItemTransactionService itemTransactionService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        Long beforePoint1 = 10000L;
+        memberRepository.save(createMember(beforePoint1));
+
+        Long beforePoint2 = 10001L;
+        memberRepository.save(createMember(beforePoint2));
+    }
 
     @DisplayName("아이템 거래 명세를 받아 거래를 생성한다.")
     @Test
@@ -27,11 +43,35 @@ class ItemTransactionServiceTest {
                 .inventoryId(1L)
                 .memberId(1L)
                 .quantity(10)
+                .itemPrice(100)
                 .build();
 
         ItemTransactionResponse response = itemTransactionService.createItemTransaction(request, now);
 
         assertThat(response.getItemTransactionId()).isEqualTo(1L);
         assertThat(response.getOrderedAt()).isEqualTo(now);
+        assertThat(response.getTotalPriceWithCharge()).isEqualTo(1000);
+        assertThat(response.getAfterMemberPoint()).isEqualTo(9000);
+    }
+
+    @DisplayName("회원의 포인트가 부족한 경우 예외가 발생한다.")
+    @Test
+    void createItemTransaction2() {
+        LocalDateTime now = LocalDateTime.now();
+        ItemTransactionRequest request = ItemTransactionRequest.builder()
+                .itemId(1L)
+                .inventoryId(1L)
+                .memberId(2L)
+                .quantity(1)
+                .itemPrice(10001)
+                .build();
+
+        assertThatThrownBy(() -> itemTransactionService.createItemTransaction(request, now))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("포인트 부족");
+    }
+
+    private Member createMember(Long point) {
+        return Member.builder().point(point).build();
     }
 }
