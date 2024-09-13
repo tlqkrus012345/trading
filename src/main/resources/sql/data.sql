6561b0474bbad038237661886d1cd41b0bc403ad
tlqkrus012345 <tlqkrus012345@naver.com>
2024-09-13 15:00:33 +0900

    item_sale_id 데이터 추가

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,8 +1,8 @@
 insert into item_sale_info(member_id, inventory_id, item_id, member_name, item_name, item_price, item_type, description, status, quantity)
-values (1, 1, 1, '유저1', '강철검', 1000, 'SWORD', '일반 강철검', 'SELLING', 1),
-       (2, 2, 2, '유저2', '목검', 500, 'SWORD', '일반 목검', 'SOLD_OUT', 2),
-       (3, 3, 3, '유저3', '다이아검', 2000, 'SWORD', '일반 다이아검', 'WAIT', 3),
-       (3, 4, 4, '유저3', '강철도끼', 1500, 'AXE', '일반 강철도끼', 'SELLING', 3);
+values (1, 1, 1, '유저1', '강철검', 1000, 'SWORD', '일반 강철검', 'SELLING', 1000),
+       (2, 2, 2, '유저2', '목검', 500, 'SWORD', '일반 목검', 'SOLD_OUT', 2000),
+       (3, 3, 3, '유저3', '다이아검', 2000, 'SWORD', '일반 다이아검', 'WAIT', 3000),
+       (3, 4, 4, '유저3', '강철도끼', 1500, 'AXE', '일반 강철도끼', 'SELLING', 30);
 
 insert into item(name, type, price, description)
 values ('강철검', 'SWORD', 7000, '일반 강철검'),
@@ -18,12 +18,12 @@ values ('가 창', 3, 1),
        ('마 검', 2, 1),
        ('바 도끼', 1, 1);
 
-insert into item_transaction(member_id, inventory_id, item_id, total_price, total_quantity, total_price_with_charge, status, ordered_at)
-values (1, 1, 1, 1000, 200, 220000, 'COMPLETED', '2024-09-8T15:43:02.842110400'),
-       (1, 1, 1, 1000, 200, 220000, 'COMPLETED', '2024-09-8T15:43:02.842110400'),
-       (1, 1, 2, 1000, 350, 385000, 'COMPLETED', '2024-09-8T15:43:02.842110400'),
-       (1, 1, 3, 1000, 100, 105000, 'COMPLETED', '2024-09-8T15:43:02.842110400'),
-       (1, 1, 4, 1000, 100, 105000, 'COMPLETED', '2024-09-8T15:43:02.842110400');
+insert into item_transaction(member_id, inventory_id, item_id, item_sale_id, total_price, total_quantity, total_price_with_charge, status, ordered_at)
+values (1, 1, 1, 1, 1000, 200, 220000, 'COMPLETED', '2024-09-8T15:43:02.842110400'),
+       (1, 1, 1, 1, 1000, 200, 220000, 'COMPLETED', '2024-09-8T15:43:02.842110400'),
+       (1, 1, 2, 2, 1000, 350, 385000, 'COMPLETED', '2024-09-8T15:43:02.842110400'),
+       (1, 1, 3, 3, 1000, 100, 105000, 'COMPLETED', '2024-09-8T15:43:02.842110400'),
+       (1, 1, 4, 4, 1000, 100, 105000, 'COMPLETED', '2024-09-8T15:43:02.842110400');
 
 insert into member(name, point, phone)
 values ('유저 1', 1000000, '010-1234-5678'),
