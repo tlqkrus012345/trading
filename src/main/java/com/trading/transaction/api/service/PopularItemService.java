29f5f4c9fdbd74ef6c2537dea1ea3acf65b377ac
tlqkrus012345 <tlqkrus012345@naver.com>
2024-12-19 19:47:16 +0900

    캐시 적용

--- src/main/java/com/trading/transaction/api/service/PopularItemService.java
+++ src/main/java/com/trading/transaction/api/service/PopularItemService.java
@@ -2,7 +2,9 @@ package com.trading.transaction.api.service;
 
 import com.trading.transaction.domain.ItemTransactionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,7 +13,10 @@ import java.util.List;
 public class PopularItemService {
 
     private final ItemTransactionRepository itemTransactionRepository;
+    private final static String POPULAR_ITEM = "POPULAR_ITEM";
 
+    @Cacheable(POPULAR_ITEM)
+    @Transactional(readOnly = true)
     public List<PopularItemResponse> getPopularItems() {
         return itemTransactionRepository.findPopularItems();
     }
