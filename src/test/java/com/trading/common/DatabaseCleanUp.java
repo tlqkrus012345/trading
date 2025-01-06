60feb87c480da8effcd6780189c02c444af88a92
tlqkrus012345 <tlqkrus012345@naver.com>
2025-01-06 15:54:51 +0900

    mysql 문법에 맞게 코드 수정

--- src/test/java/com/trading/common/DatabaseCleanUp.java
+++ src/test/java/com/trading/common/DatabaseCleanUp.java
@@ -1,7 +1,5 @@
 package com.trading.common;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,8 +8,6 @@ import java.util.List;
 
 @Component
 public class DatabaseCleanUp {
-
-    Logger logger = LoggerFactory.getLogger(DatabaseCleanUp.class);
     private final JdbcTemplate jdbcTemplate;
 
     public DatabaseCleanUp(JdbcTemplate jdbcTemplate) {
@@ -20,23 +16,18 @@ public class DatabaseCleanUp {
 
     @Transactional
     public void truncateTable() {
-        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY FALSE");
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0");
         List<String> tableNames = getTableNames();
         tableNames.forEach(tableName -> {
             String truncateQuery = "TRUNCATE TABLE `" + tableName + "`";
-            logger.info(truncateQuery);
             jdbcTemplate.execute(truncateQuery);
-
-            String alterTableQuery = "ALTER TABLE `" + tableName + "` ALTER COLUMN id RESTART WITH 1";
-            logger.info(alterTableQuery);
-            jdbcTemplate.execute(alterTableQuery);
         });
-        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY TRUE");
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1");
     }
 
     private List<String> getTableNames() {
         return jdbcTemplate.queryForList(
-                "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'",
+                "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = DATABASE()",
                 String.class
         );
     }
